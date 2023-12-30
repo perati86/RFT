ea6f283ff794e19741efb8426309bf27de62db46
perati86 <perati86@gmail.com>
2023-12-30 10:35:33 +0100

    Created database connection

---
+++ user_handling/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_TYPE','mysql');
+    define('DB_NAME','dvv1uq');
+    define('DB_HOST','localhost');
+    define('DB_USER','dvv1uq');
+    define('DB_PASS','csrT6WzxVKkTukOS');
+?>
